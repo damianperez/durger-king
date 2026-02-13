58432dafb450c8fc3607b76ae367c4ebfa1758f0
damianperez <perezdamian@gmail.com>
2026-02-13 06:58:18 -0300

    Casa

--- ElBot/hook.php
+++ ElBot/hook.php
@@ -3,7 +3,7 @@
 
 
 // Load composer
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Load all configuration options
 /** @var array $config */
