1eae9a43c58a8cd16ccfcaafd82a03d6d71e32a2
damianperez <perezdamian@gmail.com>
2026-02-14 08:51:55 -0300

    Casa

--- ElBot/hook.php
+++ ElBot/hook.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 // Load composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
--- service.php
+++ service.php
@@ -17,6 +17,7 @@ Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
 Router::resource("{$_ENV['REMOTE_URI']}/ElBot", __DIR__ . '/ElBot');
 
 Router::any("{$_ENV['REMOTE_URI']}/telegram", function () {
+    Response::send(StatusCode::OK, 'Bot is working...');
     $TgBot = require __DIR__ . '/ElBot/hook.php';
     Response::send(StatusCode::OK, 'Bot is working...');
 });
