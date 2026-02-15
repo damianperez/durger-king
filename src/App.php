f81ed1624c0f86d5f0f4e3db81c4e1a6b117cf74
damianperez <perezdamian@gmail.com>
2026-02-15 11:18:26 -0300

    Casa

--- src/App.php
+++ src/App.php
@@ -26,6 +26,7 @@ class App extends \TelegramBot\UpdateHandler {
    public function __process(Update $update): void {
 
       //Telegram::setAdminId($_ENV['ADMIN_CHAT_ID']);
+      die( var_dump($update));
       Telegram::setAdminId(662767623);
 
       if ($update->getMessage()->getText() === '/ping') {
