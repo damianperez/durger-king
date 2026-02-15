5bf08036a784a951af75710b4236f83224c4c49e
damianperez <perezdamian@gmail.com>
2026-02-15 10:32:09 -0300

    Casa

--- servicio.php
+++ servicio.php
@@ -20,46 +20,8 @@ Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
 Router::resource("{$_ENV['REMOTE_URI']}/ElBot", __DIR__ . '/ElBot');
 
 
-
 Router::any("{$_ENV['REMOTE_URI']}/telegram", function () {
-   try {
-        // Create Telegram API object
-        $config = require __DIR__ . '/ElBot/config.php';
-        $telegram = new Longman\TelegramBot\Telegram($config['api_key'], $config['bot_username']);
-
-        // Enable admin users
-        $telegram->enableAdmins($config['admins']);
-
-        // Add commands paths containing your custom commands
-        $telegram->addCommandsPaths($config['commands']['paths']);
-
-        // Enable MySQL if required
-        $telegram->enableMySql($config['mysql']);
-        // Set custom Download and Upload paths
-        $telegram->setDownloadPath($config['paths']['download']);
-        $telegram->setUploadPath($config['paths']['upload']);
-
-        // Load all command-specific configurations
-        // foreach ($config['commands']['configs'] as $command_name => $command_config) {
-        //     $telegram->setCommandConfig($command_name, $command_config);
-        // }
-
-        // Requests Limiter (tries to prevent reaching Telegram API limits)
-        $telegram->enableLimiter($config['limiter']);
-
-        // Handle telegram webhook request
-        $telegram->handle();
-
-    } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-        // Log telegram errors
-        Longman\TelegramBot\TelegramLog::error($e);
-
-        // Uncomment this to output any errors (ONLY FOR DEVELOPMENT!)
-        echo $e;
-    } catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
-        // Uncomment this to output log initialisation errors (ONLY FOR DEVELOPMENT!)
-        echo $e;
-    }
+    (new App())->resolve();
 
     Response::send(StatusCode::OK, 'Bot is working...');
 });
