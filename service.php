e6a41a7018026c939663df7cc0dabac165d77ad0
damianperez <perezdamian@gmail.com>
2026-02-15 11:03:06 -0300

    Casa

--- service.php
+++ service.php
@@ -9,18 +9,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
-
 Router::resource("{$_ENV['REMOTE_URI']}/ElBot", __DIR__ . '/ElBot');
-
-
 
 Router::any("{$_ENV['REMOTE_URI']}/telegram", function () {
     (new App())->resolve();
-
     Response::send(StatusCode::OK, 'Bot is working...');
 });
 
