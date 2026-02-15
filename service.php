f2a26039e3e0b30db2f88b21fb062650a635ba20
damianperez <perezdamian@gmail.com>
2026-02-15 11:39:01 -0300

    Casa

--- service.php
+++ service.php
@@ -7,8 +7,8 @@ use Utilities\Routing\Utils\StatusCode;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
+//ini_set('display_errors', '1');
+//error_reporting(E_ALL);
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
