9187fdd6f7e33d8e38cbddd95228c3dbca988acf
damianperez <perezdamian@gmail.com>
2026-02-16 11:04:43 -0300

    Casa

--- ElBot/config.php
+++ ElBot/config.php
@@ -32,7 +32,8 @@ return [
 
     // When using the getUpdates method, this can be commented out
     'webhook'      => [
-        'url' => 'https://bots.perezcompany.com.ar/durger-king/ElBot/hook.php',
+        //'url' => 'https://bots.perezcompany.com.ar/durger-king/ElBot/hook.php',
+        'url' => 'https://bots.perezcompany.com.ar/durger-king/telegram',
         // Use self-signed certificate
         // 'certificate'     => __DIR__ . '/path/to/your/certificate.crt',
         // Limit maximum number of connections
