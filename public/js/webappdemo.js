1a149e69855daf632b2799cc8118ea6b167a3ea3
damianperez <perezdamian@gmail.com>
2026-02-16 10:47:17 -0300

    Casa

--- public/js/webappdemo.js
+++ public/js/webappdemo.js
@@ -152,7 +152,7 @@ var DemoApp = {
         $.ajax(`${basePath}/telegram`, {
             type: 'POST',
             data: $.extend(data, {_auth: authData, method: method}),
-           // data: JSON.stringify($.extend(data, {_auth: authData, method: method})),
+            data: JSON.stringify($.extend(data, {_auth: authData, method: method})),
             dataType: 'json',            
             xhrFields: {
                 withCredentials: true
