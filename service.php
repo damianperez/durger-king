6418bdbb50451cfd7b93e2f13b1a707250c72eb6
damianperez <perezdamian@gmail.com>
2026-02-16 10:30:43 -0300

    Casa

--- service.php
+++ service.php
@@ -19,7 +19,7 @@ Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
 Router::any("{$_ENV['REMOTE_URI']}/telegram", function () {
     (new App())->resolve();
     
-    Response::send(StatusCode::OK, ['aa'=>'Bot is working...']);
+    Response::send(StatusCode::OK, ['service.php'=>'Bot is working...']);
 });
 
 Router::any("{$_ENV['REMOTE_URI']}", function () {
