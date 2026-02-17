379083c7272fe717d7cc558bc52d7fae6529736c
damianperez <perezdamian@gmail.com>
2026-02-17 08:55:57 -0300

    Casa

--- public/index.php
+++ public/index.php
@@ -535,7 +535,7 @@
 <script>
     Cafe.init({        
 		"apiUrl": "https://bots.perezcompany.com.ar/durger-king/telegram",
-//		"mode":"menu",
+		"mode":"inline",
         "userId": 0,
         "userHash": null
     });
