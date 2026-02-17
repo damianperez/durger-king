983fdfe6cad90a0ceba8469628ff69db22e55a16
damianperez <perezdamian@gmail.com>
2026-02-17 09:58:17 -0300

    Casa

--- src/App.php
+++ src/App.php
@@ -23,9 +23,24 @@ class App extends \TelegramBot\UpdateHandler {
     * @param Update $update
     * @return void
     */
+    public static function debug_a_admins(   $quien, $msg )
+    {
+		$bot_api_key  = "676438755:AAG3QBJ5owYiwMjV2wiluXIJB5DGxFyjKbY";
+		$bot_username = '@Buchonbot';
+		$chatIds = array("662767623"); // Los destinatarios 
+    
+    	foreach ($chatIds as $chatId) {
+        $data = array(   'chat_id' => $chatId,
+        'text' => 'Debug '.$quien. '  '.var_export($msg,true) ,
+        'parse_mode' => 'HTML' );
+         $response = file_get_contents("https://api.telegram.org/bot$bot_api_key/sendMessage?" . http_build_query($data) );
+    	}
+    	return ; 
+    }
    public function __process(Update $update): void {
       //Telegram::setAdminId($_ENV['ADMIN_CHAT_ID']);
       //die( var_dump($update));
+      $this->debug_a_admins('Update', $update);
       Telegram::setAdminId(662767623);
       if ($update->getMessage()->getText() === '/ping') {
          Request::sendMessage([
