39783326afcbf9f65f4c1436c878e3ee1fc24dc2
damianperez <perezdamian@gmail.com>
2026-04-03 13:04:53 -0300

    abr

--- public/js/webappdemo.js
+++ public/js/webappdemo.js
@@ -142,7 +142,7 @@ var DemoApp = {
 
     apiRequest: function (method, data, onCallback) {
         const authData = DemoApp.initData || '';
-        const basePath = window.location.pathname.split('/').slice(0, -2).join('/');
+        const basePath = window.location.pathname.split('/').slice(0, -3).join('/');
         
         //console.log($.extend(data, {_auth: authData, method: method}));
         var jsonConvertedData = JSON.stringify($.extend(data, {_auth: authData, method: method}));
