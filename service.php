60f5e24a0534c49215d8500d9d450b1292517792
damianperez <perezdamian@gmail.com>
2026-04-03 12:10:05 -0300

    abr

--- service.php
+++ service.php
@@ -14,12 +14,12 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
 Router::resource("{$_ENV['REMOTE_URI']}/ElBot", __DIR__ . '/ElBot');
-Router::any("{$_ENV['REMOTE_URI']}/telegram", function () {
+Router::resource("{$_ENV['REMOTE_URI']}/telegram", function () {
     //Response::send(StatusCode::OK, (new App())->resolve());
-    (new App())->resolve();
-    Response::send(StatusCode::OK);
+    //(new App())->resolve();
+    //Response::send(StatusCode::OK);
    // Response::send(StatusCode::OK);
-    //Response::send(StatusCode::OK, ['service.php'=>'Bot is working...']);
+    Response::send(StatusCode::OK, ['service.php'=>'Bot is working...']);
 });
 Router::any("{$_ENV['REMOTE_URI']}", function () {
     echo "Ready to serve...";
