29776a12afb358ae3234448474fb3039086f6360
damianperez <perezdamian@gmail.com>
2026-04-03 12:24:46 -0300

    abr

--- service.php
+++ service.php
@@ -9,11 +9,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 //ini_set('display_errors', '1');
 //error_reporting(E_ALL);
+#REMOTE_BASE_PATH="https://bots.perezcompany.com.ar" # Without trailing slash
+#REMOTE_URI="/durger-king" # Leave empty if your in the root of your domain (With trailing slash)
+
+#REMOTE_PATH="${REMOTE_BASE_PATH}${REMOTE_URI}" # DO NOT CHANGE THIS
+#RESOURCE_PATH="${REMOTE_PATH}/public/" # DO NOT CHANGE THIS
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 Router::resource("{$_ENV['REMOTE_URI']}", __DIR__ . '/public');
-#Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
+Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
 Router::resource("{$_ENV['REMOTE_URI']}/ElBot", __DIR__ . '/ElBot');
 
 Router::any("/durger-king/public/telegram", function () {
