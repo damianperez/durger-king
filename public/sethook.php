ab8140df8552217e33cf6cebc6d1469b0843a3e8
Damian <perezdamian@gmail.com>
2026-04-15 14:11:41 -0300

    pruebas

--- public/sethook.php
+++ public/sethook.php
@@ -2,9 +2,9 @@
 
 $result = \TelegramBot\Request::setWebhook([
     'bot_token' => $_ENV['TELEGRAM_BOT_TOKEN'],
-    'url' => $_ENV['REMOTE_PATH'] . '/telegram',
+    'url' => $_ENV['REMOTE_PATH'] . '/pruebas',
     'drop_pending_updates' => true,
 ]);
 
-echo $_ENV['REMOTE_PATH'] . '/telegram' . PHP_EOL;
+echo $_ENV['REMOTE_PATH'] . '/pruebas' . PHP_EOL;
 echo $result->isOk() ? 'Webhook set successfully!' : 'Webhook set failed!';
