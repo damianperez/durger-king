89ac9861476e2e8452f0b456f5b908ad4de59d20
Damian Perez <perezdamian@gmail.com>
2026-06-29 12:29:54 -0300

    nada

--- service.php
+++ service.php
@@ -1,18 +1,13 @@
 <?php declare(strict_types=1);
-
 use ShahradElahi\DurgerKing\App;
 use Utilities\Routing\Response;
 use Utilities\Routing\Router;
 use Utilities\Routing\Utils\StatusCode;
-
 require_once __DIR__ . '/vendor/autoload.php';
-
 ini_set('display_errors', '0');
 error_reporting(E_ERROR);
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-
 Router::resource("{$_ENV['REMOTE_URI']}/public", __DIR__ . '/public');
 
 Router::any("{$_ENV['REMOTE_URI']}/telegram", function () {
