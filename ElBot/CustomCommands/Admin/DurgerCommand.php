932d5ed0f0752148ecd53c0c5366fd4f6e3b7891
Damian Perez <perezdamian@gmail.com>
2026-07-15 13:07:16 -0300

    remontando ElBot

--- ElBot/CustomCommands/Admin/DurgerCommand.php
+++ ElBot/CustomCommands/Admin/DurgerCommand.php
@@ -61,12 +61,12 @@ class DurgerCommand extends UserCommand
         $data['user_id']  = $message->getFrom()->getId();
         $data['chat_id']  = $chat_id;
         //$this->debug_a_admins('Vino ', $message );
-        /*
+        
         $wdata = $this->getMessage()?->getWebAppData()?->getData();
         if ($wdata) {
             $this->debug_a_admins(   'Webapp', $wdata );
         }
-        */
+        
         /**
          * Catch and handle any service messages here.
          */
