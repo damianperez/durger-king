1cde6451b20c561a3f94c6c75b78e2b941a6d125
Damian Perez <perezdamian@gmail.com>
2026-07-15 12:44:47 -0300

    remontando ElBot

--- ElBot/CustomCommands/Admin/DurgerCommand.php
+++ ElBot/CustomCommands/Admin/DurgerCommand.php
@@ -55,9 +55,9 @@ class DurgerCommand extends SystemCommand
         $chat_id = $chat->getId();
         $user_id = $user->getId();
         // Preparing response
-        $data = [
-            'chat_id'      => $chat_id,            
-        ];
+        
+        $data['user_id']  = $message->getFrom()->getId();
+        $data['chat_id']  = $chat_id;
         $this->debug_a_admins('Vino ', $message );
         $wdata = $this->getMessage()?->getWebAppData()?->getData();
         if ($wdata) {
