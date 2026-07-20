09f62bdd4ab53d23bcaf277d4dfede25263d8142
damianperez <perezdamian@gmail.com>
2026-07-20 17:43:17 -0300

    gmc

--- src/Plugins/WebService.php
+++ src/Plugins/WebService.php
@@ -28,6 +28,7 @@ class WebService extends \TelegramBot\Plugin
      */
     public function onWebAppData(WebAppData $webAppData): \Generator
     {
+        die(var_dump($webAppData->getRawData()));
         if ($webAppData->getRawData()['method'] == "makeOrder") {
             header('Content-Type: application/json');
 
