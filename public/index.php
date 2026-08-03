2f45881de6567212101f0f0ed20b811b9a7c686b
Damian Perez <perezdamian@gmail.com>
2026-08-03 11:58:50 -0300

    servicio

--- public/index.php
+++ public/index.php
@@ -586,10 +586,13 @@ en tg original, pero en mi servidor devuelve
 <!--
 <script src="js/tgsticker.js?27"></script>
 <script src="js/cafe.js?version=<?php echo uniqid() ?>"></script>
+
+
+ //"apiUrl": "<?php echo $_ENV['REMOTE_PATH'] . '/telegram' ?>",
 -->
 <script>
     Cafe.init({
-        //"apiUrl": "<?php echo $_ENV['REMOTE_PATH'] . '/telegram' ?>",
+       
 		"apiUrl": 'https://bots.perezcompany.com.ar/ontheball/hook_durger.php',
         "userId": 0,
 		"mode": "menu", // "menu" or "order"
