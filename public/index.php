4cb19195e475726fd6cfd192069062d1ff4c0f2c
Damian Perez <perezdamian@gmail.com>
2026-08-03 14:38:24 -0300

    servicio

--- public/index.php
+++ public/index.php
@@ -536,6 +536,7 @@
   Telegram.WebApp.openInvoice = function (url, callback) {
     var a = document.createElement('A'), match, slug;
     a.href = url;
+	slug = 'durger-' + Math.random().toString(36).substring(2, 15);
     if (a.protocol != 'http:' &&
         a.protocol != 'https:' 
     ) {
